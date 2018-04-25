388b57eb270082067a08c04780c8ae939ee2eab4
Sergio Marchena <mar16387@uvg.edu.gt>
2018-04-25 09:31:15 -0600

    avance en clase
    falta escena 2, sonido, colisiones

---
+++ Assets/Scripts/Creador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creador : MonoBehaviour {
+
+	public GameObject aste;
+	float tiempo, frecuencia;
+	Vector3 posicion = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		frecuencia = 3f;
+		tiempo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (tiempo > frecuencia) {
+			Instantiate (aste, transform.position, Quaternion.identity, transform);
+			tiempo = 0;
+		} else {
+			tiempo += Time.deltaTime;
+
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour {
+	
+	Vector3 direccion = new Vector3(1f,1f,0f);
+	Vector3 transportar= new Vector3(0f,0f,0f);
+
+	float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		velocidad = Random.Range (1f, 5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(direccion * Time.deltaTime * velocidad);
+
+		if (transform.position.x < -5) {
+			transportar.x=5;
+			transportar.y=transform.position.y;
+			transform.position=transportar;
+		} else if (transform.position.x > 5) {
+			transportar.x=-5;
+			transportar.y=transform.position.y;
+			transform.position=transportar;
+		}else if(transform.position.y<-5){
+			transportar.y=5;
+			transportar.x=transform.position.x;
+			transform.position=transportar;
+		}else if(transform.position.y>5){
+			transportar.y=-5;
+			transportar.x=transform.position.x;
+			transform.position=transportar;
+		}
+	}
+}
